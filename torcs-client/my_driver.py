79b1da3ff0c77dc39b625fb9b0792dbdccc0af7d
akashrajkn <akashrajkn@gmail.com>
2017-11-27 11:46:51 +0100

    Bookkeeping changes 1. Use os.dirname.realpath to save and load models 2. Separated classes from training functions 3. Renamed saved models

--- torcs-client/my_driver.py
+++ torcs-client/my_driver.py
@@ -1,30 +1,31 @@
+import os
 import torch
 import numpy as np
 import dill as pickle
 
 from torch.autograd import Variable
 
-from pytocl.nn_linear_regression import LinearRegression
-from pytocl.pyESN_for_TORCS import ESN
 from pytocl.driver import Driver
 from pytocl.car import State, Command
+
+from pytocl.neural_net import FeedForwardRegression
+# from pytocl.echo_state_network import ESN
 
 
 class MyDriver(Driver):
     def __init__(self):
-        input_size = 21
-        hidden_size = 40
-        output_size = 3
-        self.nn_model = LinearRegression(input_size, output_size, hidden_size)
-        # self.nn_model.load_state_dict(torch.load('../../models/model_nn_regression.pkl'))
+        realpath = os.path.dirname(os.path.realpath(__file__))
 
-        # ESN
-        with open('/home/akashrajkn/Documents/github_projects/ruimte-auto/torcs-client/pytocl/esn.pkl', 'rb') as in_strm:
-            datastruct = pickle.load(in_strm)
+        self.nn_model = FeedForwardRegression()
+        self.nn_model.load_state_dict(torch.load(realpath + '/../models/neural_net_regression.pkl'))
 
-        self.esn = datastruct
-        self.reservoir = np.zeros(200)
-        self.control = np.zeros(3)
+        # # ESN
+        # with open('realpath + '/../models/esn.pkl', 'rb') as f:
+        #     esn_binary = pickle.load(f)
+        #
+        # self.esn =  esn_binary
+        # self.reservoir = np.zeros(200)
+        # self.control = np.zeros(3)
 
     def reservoir_computing(self, carstate, command):
 
@@ -39,7 +40,7 @@ class MyDriver(Driver):
 
     def convert_carstate_to_array(self, carstate):
         '''
-        Convert the carstate to numpy array
+        Convert the carstate to np array
         '''
         speed = carstate.speed_x
         track_position = carstate.distance_from_center
@@ -47,28 +48,29 @@ class MyDriver(Driver):
 
         sensors = list(carstate.distances_from_edge)
         # FIXME: NN model does not use one of the sensors
-        # sensors = sensors[:-1]
-
+        sensors = sensors[:-1]
         carstate_array = np.array([[speed, angle, track_position] + sensors], dtype=np.float32)
+
+        # carstate_array = np.array([speed, angle, track_position] + sensors)
 
         return carstate_array
 
     def drive(self, carstate: State) -> Command:
-        # # NN_MODEL
-        # x_test = self.convert_carstate_to_array(carstate)
-        # predicted = self.nn_model(Variable(torch.from_numpy(x_test))).data.numpy()
-        #
-        # command = Command()
-        #
-        # command.accelerator = predicted[0][0]
-        # command.brake = predicted[0][1]
-        # command.steering = predicted[0][2]
-        #
-        # # if carstate.rpm < 2500:
-        # #     command.gear = carstate.gear - 1
+        # NN_MODEL
+        x_test = self.convert_carstate_to_array(carstate)
+        predicted = self.nn_model(Variable(torch.from_numpy(x_test))).data.numpy()
 
         command = Command()
-        command = self.reservoir_computing(carstate, command)
+
+        command.accelerator = predicted[0][0]
+        command.brake = predicted[0][1]
+        command.steering = predicted[0][2]
+
+        # if carstate.rpm < 2500:
+        #     command.gear = carstate.gear - 1
+
+        # command = Command()
+        # command = self.reservoir_computing(carstate, command)
 
         if not command.gear:
             command.gear = carstate.gear or 1
